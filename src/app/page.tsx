e2733e784db0cb108e5a3aca7b39e3fa26f0a278
mikohan <angara99@gmail.com>
2025-11-22 16:12:37 -0800

    Added no index

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,9 +8,15 @@ import {
   CallToAction,
   Footer,
 } from "@/sections";
+
+import Head from "next/head";
+
 export default function Home() {
   return (
     <>
+      <Head>
+        <meta name="robots" content="noindex" />
+      </Head>
       <Navbar />
       <Hero />
       <LogoTicker />
